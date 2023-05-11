8e72b9c2209afe48720d50c45d69c0619ccdf038
simple710 <112814424+simple0710@users.noreply.github.com>
2023-05-11 10:23:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
+app.get('/', (res, req) => {
+  res.send("Hello");
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
